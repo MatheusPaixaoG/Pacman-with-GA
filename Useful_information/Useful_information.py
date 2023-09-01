d44f9da0d3306895243acc1daf5ede4ed69406d6
Matheus Paixão <mpgs@cin.ufpe.br>
2023-09-01 17:10:24 -0300

    Calculate vectors from ghosts to pacman

--- Useful_information/Useful_information.py
+++ Useful_information/Useful_information.py
@@ -2,6 +2,18 @@ class UsefulInformation():
     def __init__(self, game_controller):
         self.gc = game_controller
     
+    def updateDistsToGhosts(self):
+        self.distFromPinky = self.gc.pacman.position.manhattanDistTo(self.gc.ghosts.pinky.position)
+        self.distFromInky = self.gc.pacman.position.manhattanDistTo(self.gc.ghosts.inky.position)
+        self.distFromClyde = self.gc.pacman.position.manhattanDistTo(self.gc.ghosts.clyde.position)
+        self.distFromBlinky = self.gc.pacman.position.manhattanDistTo(self.gc.ghosts.blinky.position)
+    
+    def updateVectorsFromGhostsToPac(self):
+        self.vecToPinky = self.gc.ghosts.pinky.position.__sub__(self.gc.pacman.position)
+        self.vecToInky = self.gc.ghosts.inky.position.__sub__(self.gc.pacman.position)
+        self.vecToClyde = self.gc.ghosts.clyde.position.__sub__(self.gc.pacman.position)
+        self.vecToBlinky = self.gc.ghosts.blinky.position.__sub__(self.gc.pacman.position)
+
     def printGhostsPositions(self):
         print(f"PINKY POS: {self.gc.ghosts.pinky.position}")
         print(f"INKY POS: {self.gc.ghosts.inky.position}")
@@ -13,12 +25,13 @@ class UsefulInformation():
         print(f"DIST FROM INKY: {self.distFromInky}")
         print(f"DIST FROM CLYDE: {self.distFromClyde}")
         print(f"DIST FROM BLINKY: {self.distFromBlinky}")
-    
-    def updateDistsToGhosts(self):
-        self.distFromPinky = self.gc.pacman.position.distTo(self.gc.ghosts.pinky.position)
-        self.distFromInky = self.gc.pacman.position.distTo(self.gc.ghosts.inky.position)
-        self.distFromClyde = self.gc.pacman.position.distTo(self.gc.ghosts.clyde.position)
-        self.distFromBlinky = self.gc.pacman.position.distTo(self.gc.ghosts.blinky.position)
+
+    def printVectorsToPac(self):
+        print(f"VECTOR TO PINKY: {self.vecToPinky}")
+        print(f"VECTOR TO INKY: {self.vecToInky}")
+        print(f"VECTOR TO CLYDE: {self.vecToClyde}")
+        print(f"VECTOR TO BLINKY: {self.vecToBlinky}")
         
     def update(self):
         self.updateDistsToGhosts()
+        self.updateVectorsFromGhostsToPac()
