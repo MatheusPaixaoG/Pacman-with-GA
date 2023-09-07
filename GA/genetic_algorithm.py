6d7ce2c634e0170ca9c043711efe5f18f9cc5c72
bruhenri12 <bhsm@cin.ufpe.br>
2023-09-07 01:32:48 -0300

    GA: adding the Genetic Manager class

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -1,3 +1,11 @@
+import copy
+import random
+import sys
+sys.path.append("../Useful_information")
+
+# created classes/utils
+from params_reader import WEIGHTS
+
 class Individual(object):
     def __init__(self, dna, rna):
         self.dna = dna
@@ -9,3 +17,16 @@ class Individual(object):
     
     def fitness(self):
         return "PONTUAÇÃO FINAL"
+
+class GeneticManager:
+
+    def random_dna_generator(self):
+        return [random.uniform(i[0],i[1]) for i in WEIGHTS.values()]
+    
+    def crossover(self, parents, offspring_size=1, splits=0):
+        offspring = []
+        
+        for o in range(offspring_size):
+            base_parent = parents[o % len(parents)]
+            child = copy.deepcopy(base_parent)
+            offspring.append(child)
