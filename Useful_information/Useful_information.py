2861919d369aaedf4eb890f9233bb11b6e9d9424
bruhenri12 <bhsm@cin.ufpe.br>
2023-09-07 01:17:25 -0300

    Adding power mode information

--- Useful_information/Useful_information.py
+++ Useful_information/Useful_information.py
@@ -1,11 +1,20 @@
+import sys
+sys.path.append("../Pacman_Complete")
+
+from Pacman_Complete.constants import *
+
 class UsefulInformation():
     def __init__(self, game_controller):
         self.gc = game_controller
+
+        self.powerMode = False
+        self.powerPelletsAvailable = False
 
         self.iVecToInky = None
         self.iVecToPinky = None
         self.iVecToClyde = None
         self.iVecToBlinky = None
+        self.vecToNearestGhost = None
     
     def updateDistsToGhosts(self):
         self.distFromPinky = self.gc.pacman.position.manhattanDistTo(self.gc.ghosts.pinky.position)
@@ -69,19 +78,28 @@ class UsefulInformation():
         self.iVecToClyde = self.vecToClyde.normalized() * (100/self.distFromClyde)
         self.iVecToBlinky = self.vecToBlinky.normalized() * (100/self.distFromBlinky)
     
-    def nearestGhost(self):
+    def updateNearestGhost(self):
         min_distance = min([self.distFromBlinky,self.distFromClyde,self.distFromInky,self.distFromPinky])
 
-        for ghost in self.gc.ghosts.ghosts:
+        for ghost in self.gc.ghosts:
             ghost_distance = self.gc.pacman.position.manhattanDistTo(ghost.position)
             if (ghost_distance == min_distance):
-                return ghost.position.__sub__(self.gc.pacman.position)
+                self.vecToNearestGhost = ghost.position.__sub__(self.gc.pacman.position)
     
     def updateResultantVecFromGhosts(self):
         self.resultantGhostVec = self.iVecToBlinky + self.iVecToInky + self.iVecToPinky + self.iVecToClyde
 
     def updateFinalResultantVector(self):
         self.finalResultantVec = self.resultantGhostVec + self.vecToPellet
+    
+    def updatePowerPelletsAvailability(self):
+        self.powerPelletsAvailable = True if self.gc.pellets.powerpellets != [] else False
+
+    def updatePowerModeStatus(self):
+        self.powerMode = False
+        for ghost in self.gc.ghosts:
+            if ghost.mode.current == FREIGHT:
+                self.powerMode = True
         
     def update(self):
         self.updateDistsToGhosts()
@@ -89,5 +107,9 @@ class UsefulInformation():
         self.updateInverseGhostsVectors()
         self.updateVectorClosestPellet()
         self.updateVectorClosestPowerPellet()
+        self.updateNearestGhost()
         self.updateResultantVecFromGhosts()
         self.updateFinalResultantVector()
+        self.updatePowerPelletsAvailability()
+        self.updatePowerModeStatus()
+    
