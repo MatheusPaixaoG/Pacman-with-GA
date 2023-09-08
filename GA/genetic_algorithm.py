07dc1c1d8705d0e4457caccba1c2e6ab37add38c
bruhenri12 <bhsm@cin.ufpe.br>
2023-09-08 20:54:35 -0300

    update: DNA structure
    Now the genotype depends on wheter or not the player is on power mode

--- GA/genetic_algorithm.py
+++ GA/genetic_algorithm.py
@@ -1,26 +1,40 @@
 import copy
 import random
+import sys
+sys.path.append("../Pacman_Complete")
 
 # created classes/utils
-from params_reader import WEIGHTS
-
+from Pacman_Complete.constants import *
+from params_reader import WEIGHTS_RANGES
 
 class Individual(object):
     def __init__(self, dna, rna):
-        self.dna = dna
+        self.dna = dna   # dict {'normal_weights': list, 'powered_weights': list}
         self._rna = rna  # dict {'Vector2': list, 'bool': list}
 
     def genotype(self):
-        rna_vector2 = [ self._rna['Vector2'][i] * self.dna[i] for i in range(len(self.dna)) ]
-        return rna_vector2 # + self._rna['bool']
+        if not self._rna['bool'][0]:  # powerMode == False
+            # Use normal weights
+            return [ self._rna['Vector2'][i] * self.dna['normal_weights'][i] for i in range(len(self.dna)) ]
+        else:
+            # Use power mode weights
+            return [ self._rna['Vector2'][i] * self.dna['powered_weights'][i] for i in range(len(self.dna)) ]
     
+    def action(self):
+        genotype = self.genotype()
+        actionVec = sum(genotype)
+        return actionVec
+        
     def fitness(self):
         return "PONTUAÇÃO FINAL"
 
 class GeneticManager:
 
     def random_dna_generator(self):
-        return [random.uniform(i[0],i[1]) for i in WEIGHTS.values()]
+        return {
+            'normal_weights': [random.uniform(i[0],i[1]) for i in WEIGHTS_RANGES['normal_weights'].values()],
+            'powered_weights': [random.uniform(i[0],i[1]) for i in WEIGHTS_RANGES['powered_weights'].values()]
+        }
     
     def crossover(self, parents, offspring_size=1, splits=0):
         offspring = []
--- GA/params_reader.py
+++ GA/params_reader.py
@@ -1,9 +1,10 @@
 import json
 
-WEIGHTS = {}
+WEIGHTS_RANGES = {}
 
 params_path = input("PATH of the params file: ")
 
 with open(params_path) as f:
     params = json.load(f)
-    WEIGHTS = params['weight_ranges'][0]
+    WEIGHTS_RANGES.update({'normal_weights':params['normal_weights']})
+    WEIGHTS_RANGES.update({'powered_weights':params['powered_weights']})
