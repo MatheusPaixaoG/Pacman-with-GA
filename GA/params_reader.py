fa0ce405211ede27014ff72e8829bd3f092e64d1
bruhenri12 <bhsm@cin.ufpe.br>
2023-09-11 01:07:58 -0300

    Updating parameters for Crossover

--- GA/params_reader.py
+++ GA/params_reader.py
@@ -1,10 +1,16 @@
 import json
 
 WEIGHTS_RANGES = {}
+CROSSOVER = {}
 
 params_path = input("PATH of the params file: ")
 
 with open(params_path) as f:
     params = json.load(f)
+
     WEIGHTS_RANGES.update({'normal_weights':params['normal_weights']})
     WEIGHTS_RANGES.update({'powered_weights':params['powered_weights']})
+
+    
+    CROSSOVER = params['crossover']
+    print(list(WEIGHTS_RANGES['normal_weights'].values()))
