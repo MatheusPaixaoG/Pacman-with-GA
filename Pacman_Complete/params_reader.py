71f5247a3889d609d76a73028b7a91183db20b03
bruhenri12 <bhsm@cin.ufpe.br>
2023-09-12 17:16:49 -0300

    Updating parameters script and file

--- Pacman_Complete/params_reader.py
+++ Pacman_Complete/params_reader.py
@@ -1,12 +1,13 @@
 import json
 
 CROSSOVER = {}
+MUTATION = {}
 POPULATION = {}
 RUN = {}
 WEIGHTS_RANGES = {}
 
 def load_params():
-    global CROSSOVER, POPULATION, RUN, WEIGHTS_RANGES
+    global CROSSOVER, MUTATION, POPULATION, RUN, WEIGHTS_RANGES
     params_path = "./params.json"
     with open(params_path) as f:
         params = json.load(f)
@@ -14,7 +15,10 @@ def load_params():
         WEIGHTS_RANGES.update({'normal_weights':params['normal_weights']})
         WEIGHTS_RANGES.update({'powered_weights':params['powered_weights']})
 
-        POPULATION = params['population']
         CROSSOVER = params['crossover']
+        MUTATION = params['mutation']
+        POPULATION = params['population']
         RUN = params['run']
     f.close()
+
+load_params()
